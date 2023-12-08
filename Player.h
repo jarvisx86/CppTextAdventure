46a36969e0317376877982a7c1e4d07408b0618f
Kyle Hardy <kyleahardy@gmail.com>
2023-12-07 19:38:06 -0500

    add getter and setter functions for m_pCurrentRoom

--- Player.h
+++ Player.h
@@ -1,15 +1,21 @@
 #pragma once
 #include <string>
 #include "Entity.h"
+#include "Room.h"
 
 class Player : public Entity
 {
 private:
 	std::string m_name;
+	const Room* m_pCurrentRoom;
 
 public:
 	std::string GetPlayerName();
 
 	void SetPlayerName(std::string playerName);
+
+	void SetCurrentRoom(const Room* pCurrentRoom);
+
+	const Room* GetCurrentRoom() const;
 };
 
