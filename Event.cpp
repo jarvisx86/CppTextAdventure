6f0bdbc3d63a73941eaaec428003429a962015ef
Kyle Hardy <kyleahardy@gmail.com>
2023-12-30 20:51:22 -0500

    add event classes

---
+++ Event.cpp
@@ -0,0 +1,45 @@
+#include "Event.h"
+#include "EventHandler.h"
+
+Event::Event(EventID eventId)
+	: m_id{eventId}
+{}
+
+void Event::Send()
+{
+	for (auto& listener : m_listeners)
+	{
+		if (listener != nullptr)
+		{
+			listener->HandleEvent(this);
+		}
+	}
+}
+
+void Event::SendToHandler(EventHandler& eventHandler)
+{
+	auto found = std::find(m_listeners.begin(), m_listeners.end(), &eventHandler);
+	if (found != m_listeners.end())
+	{
+		(*found)->HandleEvent(this);
+	}
+}
+
+void Event::AttachListener(EventHandler& eventHandler)
+{
+	m_listeners.push_back(&eventHandler);
+}
+
+void Event::DetachListener(EventHandler& eventHandler)
+{
+	for (EventHandlersListIterator iter = m_listeners.begin();
+		iter != m_listeners.end();
+		++iter)
+	{
+		if (&eventHandler == *iter)
+		{
+			m_listeners.erase(iter);
+			break;
+		}
+	}
+}
